092cb598c5eb6ba42ea0fe8074a7599bfb25df07
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-04-08 18:54:06 +0200

    change port

--- server/server.js
+++ server/server.js
@@ -45,4 +45,4 @@ app.post('/', async (req, res) => {
   }
 })
 
-app.listen(80, () => console.log('AI server started on http://localhost:5000'))
+app.listen(443, () => console.log('AI server started on http://localhost:5000'))
